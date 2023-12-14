98b3bd87fc09cfc91d22dab507816a0c3d8932a5
Hamid Zadeh <hamidmamadzade@gmail.com>
2023-12-15 00:57:42 +0100

    Add typography plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+// const plugin = require("tailwindcss/plugin");
 module.exports = {
 	content: [
 		"./_drafts/**/*.html",
@@ -10,11 +11,46 @@ module.exports = {
 	theme: {
 		theme: {
 			extend: {
-				// fontFamily: {
-				// 	sans: ["jura-regular", "sans-serif"],
-				// },
+				typography: (theme) => ({
+					default: {
+						css: {
+							pre: {
+								color: theme("colors.grey.1000"),
+								backgroundColor: theme("colors.grey.100"),
+							},
+							"pre code::before": {
+								"padding-left": "unset",
+							},
+							"pre code::after": {
+								"padding-right": "unset",
+							},
+							code: {
+								backgroundColor: theme("colors.grey.100"),
+								color: "#DD1144",
+								fontWeight: "400",
+								"border-radius": "0.25rem",
+							},
+							"code::before": {
+								content: '""',
+								"padding-left": "0.25rem",
+							},
+							"code::after": {
+								content: '""',
+								"padding-right": "0.25rem",
+							},
+						},
+					},
+				}),
 			},
 		},
 	},
-	plugins: [],
+	plugins: [
+		require("@tailwindcss/typography"),
+		require("tailwindcss/plugin")(function ({ addVariant }) {
+			addVariant(
+				"prose-inline-code",
+				'&.prose :where(:not(pre)>code):not(:where([class~="not-prose"] *))',
+			);
+		}),
+	],
 };
