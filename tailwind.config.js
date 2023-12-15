53952cd3b0991ae437ff19ab2959cee5a9f76fa6
Hamid Zadeh <hamidmamadzade@gmail.com>
2023-12-15 22:52:44 +0100

    Configure typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-// const plugin = require("tailwindcss/plugin");
 module.exports = {
 	content: [
 		"./_drafts/**/*.html",
@@ -9,48 +8,126 @@ module.exports = {
 		"./*.html",
 	],
 	theme: {
-		theme: {
-			extend: {
-				typography: (theme) => ({
-					default: {
-						css: {
-							pre: {
-								color: theme("colors.grey.1000"),
-								backgroundColor: theme("colors.grey.100"),
+		extend: {
+			typography: (theme) => ({
+				DEFAULT: {
+					css: {
+						color: theme("colors.gray.200"),
+						a: {
+							color: theme("colors.green.300"),
+							"&:hover": {
+								color: theme("colors.green.400"),
 							},
-							"pre code::before": {
-								"padding-left": "unset",
-							},
-							"pre code::after": {
-								"padding-right": "unset",
-							},
-							code: {
-								backgroundColor: theme("colors.grey.100"),
-								color: "#DD1144",
-								fontWeight: "400",
-								"border-radius": "0.25rem",
-							},
-							"code::before": {
-								content: '""',
-								"padding-left": "0.25rem",
-							},
-							"code::after": {
-								content: '""',
-								"padding-right": "0.25rem",
+							"&:visited": {
+								color: theme("colors.green.400"),
 							},
 						},
+						"a:not([href])": {
+							color: theme("colors.gray.200"),
+							textDecoration: "none",
+						},
+						blockquote: {
+							color: theme("colors.gray.200"),
+							fontWeight: "600",
+							fontStyle: "italic",
+							borderLeftColor: theme("colors.gray.400"),
+						},
+						code: {
+							color: "rgb(255, 180, 84)",
+							backgroundColor: theme("colors.gray.800"),
+							fontWeight: "400",
+							padding: "1px 3px",
+							borderRadius: "3px",
+						},
+						"code::before": {
+							content: null,
+						},
+						"code::after": {
+							content: null,
+						},
+						"a code": {
+							color: theme("colors.green.400"),
+						},
+						h1: {
+							color: theme("colors.gray.100"),
+						},
+						h2: {
+							color: theme("colors.gray.100"),
+						},
+						h3: {
+							color: theme("colors.gray.100"),
+						},
+						h4: {
+							color: theme("colors.gray.100"),
+						},
+						strong: {
+							color: theme("colors.gray.50"),
+						},
 					},
-				}),
-			},
+				},
+				dark: {
+					css: {
+						color: theme("colors.gray.300"),
+						a: {
+							color: theme("colors.primary.500"),
+							"&:hover": {
+								color: `${theme("colors.primary.400")} !important`,
+							},
+							code: { color: theme("colors.primary.400") },
+						},
+						h1: {
+							fontWeight: "700",
+							letterSpacing: theme("letterSpacing.tight"),
+							color: theme("colors.gray.100"),
+						},
+						h2: {
+							fontWeight: "700",
+							letterSpacing: theme("letterSpacing.tight"),
+							color: theme("colors.gray.100"),
+						},
+						h3: {
+							fontWeight: "600",
+							color: theme("colors.gray.100"),
+						},
+						"h4,h5,h6": {
+							color: theme("colors.gray.100"),
+						},
+						pre: {
+							backgroundColor: theme("colors.gray.800"),
+						},
+						code: {
+							backgroundColor: theme("colors.gray.800"),
+						},
+						details: {
+							backgroundColor: theme("colors.gray.800"),
+						},
+						hr: { borderColor: theme("colors.gray.700") },
+						"ol li::marker": {
+							fontWeight: "600",
+							color: theme("colors.gray.400"),
+						},
+						"ul li::marker": {
+							backgroundColor: theme("colors.gray.400"),
+						},
+						strong: { color: theme("colors.gray.100") },
+						thead: {
+							th: {
+								color: theme("colors.gray.100"),
+							},
+						},
+						tbody: {
+							tr: {
+								borderBottomColor: theme("colors.gray.700"),
+							},
+						},
+						blockquote: {
+							color: theme("colors.gray.100"),
+							borderLeftColor: theme("colors.gray.700"),
+						},
+					},
+				},
+			}),
 		},
 	},
-	plugins: [
-		require("@tailwindcss/typography"),
-		// require("tailwindcss/plugin")(function ({ addVariant }) {
-		// 	addVariant(
-		// 		"prose-inline-code",
-		// 		'&.prose :where(:not(pre)>code):not(:where([class~="not-prose"] *))',
-		// 	);
-		// }),
-	],
+	plugins: [require("@tailwindcss/typography")],
 };
