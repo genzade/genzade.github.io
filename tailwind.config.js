acb0e2288b4f53dfdb8796ed8d0285e21064f1e0
genzade <genzade@users.noreply.github.com>
2023-12-18 20:57:23 +0000

    deploy: 8addf3eac2b840f4629b495489c31609aef6ce81

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,65 @@ module.exports = {
 		"./*.html",
 	],
 	theme: {
-		theme: {
-			extend: {
-				// fontFamily: {
-				// 	sans: ["jura-regular", "sans-serif"],
-				// },
-			},
+		extend: {
+			typography: (theme) => ({
+				DEFAULT: {
+					css: {
+						color: theme("colors.gray.200"),
+						a: {
+							color: theme("colors.green.300"),
+							"&:hover": {
+								color: theme("colors.green.400"),
+							},
+							"&:visited": {
+								color: theme("colors.green.400"),
+							},
+						},
+						"a:not([href])": {
+							color: theme("colors.gray.200"),
+							textDecoration: "none",
+						},
+						blockquote: {
+							color: theme("colors.gray.200"),
+							fontWeight: "600",
+							fontStyle: "italic",
+							borderLeftColor: theme("colors.gray.400"),
+						},
+						code: {
+							color: "rgb(255, 180, 84)",
+							backgroundColor: theme("colors.gray.800"),
+							fontWeight: "400",
+							padding: "1px 3px",
+							borderRadius: "3px",
+						},
+						"code::before": {
+							content: null,
+						},
+						"code::after": {
+							content: null,
+						},
+						"a code": {
+							color: theme("colors.green.400"),
+						},
+						h1: {
+							color: theme("colors.gray.100"),
+						},
+						h2: {
+							color: theme("colors.gray.100"),
+						},
+						h3: {
+							color: theme("colors.gray.100"),
+						},
+						h4: {
+							color: theme("colors.gray.100"),
+						},
+						strong: {
+							color: theme("colors.gray.50"),
+						},
+					},
+				},
+			}),
 		},
 	},
-	plugins: [],
+	plugins: [require("@tailwindcss/typography")],
 };
